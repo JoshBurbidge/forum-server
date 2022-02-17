73754753a839c22c71148ea62edf68d33b9e3f9f
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2022-02-16 22:53:21 -0600

    post form, post show pages

--- client/pages/posts/[postId].jsx
+++ client/pages/posts/[postId].jsx
@@ -1,10 +1,25 @@
 import { useRouter } from "next/router";
+import axios from "axios";
+
+export async function getServerSideProps(ctx) {
+  const { params } = ctx;
+  const res = await axios.get(process.env.NEXT_PUBLIC_serverDomain + '/posts/byId/' + params.postId)
+
+  const post = res.data;
+  return {
+    props: {
+      post
+    }
+  }
+}
 
 export default function Post(props) {
 
-  const router = useRouter();
-  const { postId } = router.query;
-  return (
-    <p>Post: {postId}</p>
-  )
+  // const router = useRouter();
+  // const { postId } = router.query;
+  return (<>
+    <p>Title: {props.post.title}</p>
+    <p>Content: {props.post.content}</p>
+    <p>Author: {props.post.User.username}</p>
+  </>)
 }
--- client/pages/posts/new.jsx
+++ client/pages/posts/new.jsx
@@ -1,32 +1,47 @@
-import { Box, Container, TextField, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, TextField, Typography } from "@mui/material";
 import { useState } from "react";
-import Header from "../../components/Header";
-
+import axios from "axios";
 
 export default function NewPost(props) {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
+  const handleSubmit = async function (e) {
+    const res = await axios.post(process.env.NEXT_PUBLIC_serverDomain + '/posts/new', {
+      title: title,
+      content: content,
+      userId: 1
+    }, { withCredentials: true });
+    console.log(res);
+
+  }
 
 
   return (<>
     <Container sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
 
-      <Box component="form" maxWidth={'md'} sx={{ width: '100%' }}>
-        <Typography py={3} variant="h4">
-          Make a new post!
-        </Typography>
-        <TextField name="title" label="Title" variant="outlined"
-          onChange={e => setTitle(e.target.value)} margin='normal'
-          sx={{ width: '50%', bgcolor: 'common.white' }} autoFocus
-        />
+      <Grid container component="form" maxWidth={'md'} rowSpacing={1} pt={2} sx={{ width: '100%' }}
+        onSubmit={e => handleSubmit(e)}>
+        <Grid item xs={12}>
+          <Typography variant="h4">Make a new post!</Typography>
+        </Grid>
+        <Grid item xs={12} sm={8}>
+          <TextField name="title" label="Title" variant="outlined" fullWidth
+            onChange={e => setTitle(e.target.value)} margin='normal'
+            sx={{ bgcolor: 'common.white' }} autoFocus
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField multiline rows={6} fullWidth name="content" label="Content" variant="outlined"
+            onChange={e => setContent(e.target.value)} margin='normal'
+            sx={{ bgcolor: 'common.white' }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Button variant="contained" type="submit" size="large" fullWidth>Submit Post</Button>
+        </Grid>
 
-        <TextField multiline rows={6} fullWidth name="content" label="Content" variant="outlined"
-          onChange={e => setContent(e.target.value)} margin='normal'
-          sx={{ bgcolor: 'common.white' }}
-        />
-
-      </Box>
+      </Grid>
     </Container>
   </>);
 }
