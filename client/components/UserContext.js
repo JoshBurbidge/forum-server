1a716a0d4b69fd504e0027b8e41beb58e0c21299
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2022-03-06 12:27:37 -0600

    init server repo

--- client/components/UserContext.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-
-export const UserContext = React.createContext({ username: 'default', loggedIn: true });
-export const UserProvider = UserContext.Provider;
--- client/util/cookie.js
+++
@@ -1,7 +0,0 @@
-
-
-export function getUserIdCookie(req) {
-    //const req = ctx.req;
-    if (!req.cookies.userId) return null;
-    return "userId=" + req.cookies.userId;
-}
