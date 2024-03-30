fedcbfdc4c85ab394992e3e66550a337893e6b08
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2024-03-30 14:14:38 -0500

    sync table endpoint

--- src/helpers/sequelize.js
+++ src/helpers/sequelize.js
@@ -2,16 +2,16 @@ import Sequelize from 'sequelize';
 import { Post, postInit } from '../model/Post.js';
 import { User, userInit } from '../model/User.js';
 
-const synctable = async () => {
-  await Post.sync({ alter: true });
-  console.log('Post table synced');
+const syncTables = async () => {
   await User.sync({ alter: true });
   console.log('User table synced');
+  await Post.sync({ alter: true });
+  console.log('Post table synced');
 };
 
 function sequelizeInit() {
   // initialize sequelize
-  const seq = new Sequelize('forum', process.env.mysql_user, process.env.mysql_pass, {
+  const seq = new Sequelize('forum2', process.env.mysql_user, process.env.mysql_pass, {
     host: 'localhost',
     dialect: 'mysql'
   });
@@ -20,8 +20,6 @@ function sequelizeInit() {
   userInit(seq);
   seq.models.User.hasMany(seq.models.Post);
   seq.models.Post.belongsTo(seq.models.User);
-
-  // synctable();
 }
 
-module.exports = { sequelizeInit, synctable };
+module.exports = { sequelizeInit, syncTables };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { syncTables } from '../helpers/sequelize';
 
 const router = express.Router();
 
@@ -7,5 +8,17 @@ router.get('/', (req, res) => {
   res.send('Home page');
 });
 
+router.post('/syncTables', async (req, res) => {
+  try {
+    await syncTables();
+    console.log('synced');
+    res.status(201);
+    res.send();
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+    res.send();
+  }
+});
 
 module.exports = router;
