f12e003c71554d8d130edb78bdbad69669dbde7b
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2024-07-20 17:32:34 -0500

    destroy ecr repo

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,30 +1,30 @@
-resource "aws_ecr_repository" "server_ecr_repository" {
-  name = "forum-server"
-}
+# resource "aws_ecr_repository" "server_ecr_repository" {
+#   name = "forum-server"
+# }
 
-resource "aws_ecr_repository_policy" "server_repository_policy" {
-  repository = aws_ecr_repository.server_ecr_repository.name
-  policy     = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Sid": "registryPolicyStatement",
-        "Effect": "Allow",
-        "Principal": {
-          "AWS": [
-            "arn:aws:iam::575737149124:root",
-            "arn:aws:iam::575737149124:user/admin",
-            "arn:aws:iam::575737149124:role/forum-server-deploy"
-          ]
-        },
-        "Action": "ecr:*"
-      }
-    ]
-  }
-  EOF
-}
+# resource "aws_ecr_repository_policy" "server_repository_policy" {
+#   repository = aws_ecr_repository.server_ecr_repository.name
+#   policy     = <<EOF
+#   {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#       {
+#         "Sid": "registryPolicyStatement",
+#         "Effect": "Allow",
+#         "Principal": {
+#           "AWS": [
+#             "arn:aws:iam::575737149124:root",
+#             "arn:aws:iam::575737149124:user/admin",
+#             "arn:aws:iam::575737149124:role/forum-server-deploy"
+#           ]
+#         },
+#         "Action": "ecr:*"
+#       }
+#     ]
+#   }
+#   EOF
+# }
 
-output "ecr_repo_name" {
-  value = aws_ecr_repository.server_ecr_repository.name
-}
+# output "ecr_repo_name" {
+#   value = aws_ecr_repository.server_ecr_repository.name
+# }
