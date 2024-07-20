bc7491c319777767de0d7dc090cd4b7995016874
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2024-07-20 16:30:19 -0500

    add output

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -2,21 +2,6 @@ resource "aws_ecr_repository" "server_ecr_repository" {
   name = "forum-server"
 }
 
-# data "aws_iam_policy_document" "server_repository_policy_doc" {
-#   statement {
-#     effect = "Allow"
-#     principals {
-#       type = "AWS"
-#       identifiers = [
-#         "arn:aws:iam::575737149124:role/forum-server-deploy",
-#         "arn:aws:iam::575737149124:group/admin-group",
-#         "arn:aws:iam::575737149124:root"
-#       ]
-#     }
-#     actions = ["ecr:*"]
-#   }
-# }
-
 resource "aws_ecr_repository_policy" "server_repository_policy" {
   repository = aws_ecr_repository.server_ecr_repository.name
   policy     = <<EOF
@@ -38,4 +23,8 @@ resource "aws_ecr_repository_policy" "server_repository_policy" {
     ]
   }
   EOF
+}
+
+output "ecr_repo_name" {
+  value = aws_ecr_repository.server_ecr_repository.name
 }
