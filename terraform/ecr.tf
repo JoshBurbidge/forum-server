4e907d9bacf60e63c3e49799f99bb5a219d0b797
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2024-07-20 16:20:00 -0500

    use literal policy

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -2,26 +2,40 @@ resource "aws_ecr_repository" "server_ecr_repository" {
   name = "forum-server"
 }
 
-data "aws_iam_policy_document" "server_repository_policy_doc" {
-  statement {
-    effect = "Allow"
-    principals {
-      type = "AWS"
-      identifiers = [
-        "arn:aws:iam::575737149124:role/forum-server-deploy",
-        "arn:aws:iam::575737149124:group/admin-group",
-        "arn:aws:iam::575737149124:root"
-      ]
-    }
-    actions = ["ecr:*"]
-  }
-}
+# data "aws_iam_policy_document" "server_repository_policy_doc" {
+#   statement {
+#     effect = "Allow"
+#     principals {
+#       type = "AWS"
+#       identifiers = [
+#         "arn:aws:iam::575737149124:role/forum-server-deploy",
+#         "arn:aws:iam::575737149124:group/admin-group",
+#         "arn:aws:iam::575737149124:root"
+#       ]
+#     }
+#     actions = ["ecr:*"]
+#   }
+# }
 
 resource "aws_ecr_repository_policy" "server_repository_policy" {
   repository = aws_ecr_repository.server_ecr_repository.name
-  policy     = data.aws_iam_policy_document.server_repository_policy_doc.json
-}
-
-output "repository_policy" {
-  value = data.aws_iam_policy_document.server_repository_policy_doc.json
+  policy     = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "registryPolicyStatement",
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": [
+            "arn:aws:iam::575737149124:root",
+            "arn:aws:iam::575737149124:user/admin",
+            "arn:aws:iam::575737149124:role/forum-server-deploy"
+          ]
+        },
+        "Action": "ecr:*"
+      }
+    ]
+  }
+  EOF
 }
