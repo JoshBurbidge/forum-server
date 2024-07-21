c9b423658e2b9d1a1a72b347c384fa1a22b65ae6
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2024-07-21 09:41:27 -0500

    update secret arn

--- config/deployed.js
+++ config/deployed.js
@@ -2,6 +2,6 @@ module.exports = {
   env: 'deployed',
   databaseHost: 'forum-db-instance.ccixejdpat1b.us-east-1.rds.amazonaws.com',
   secrets: {
-    databaseCredentials: "arn:aws:secretsmanager:us-east-1:575737149124:secret:rds!db-4cdd19b8-52a2-490a-83f8-fa437e1ee598-pUflCa",
+    databaseCredentials: "arn:aws:secretsmanager:us-east-1:575737149124:secret:rds!db-6035f1d0-ab4e-4f9b-b7b6-1edcd9d96a8a-DW4W51",
   }
 };
