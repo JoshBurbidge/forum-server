0c3af2d6458008ec99c95e4c0f1c56b92edab885
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2024-08-03 21:04:44 -0500

    add https listener

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -24,6 +24,28 @@ resource "aws_lb_listener" "lb_listener" {
   }
 }
 
+data "aws_lb_listener" "lb_https_listener" {
+  load_balancer_arn = aws_lb.main_alb.arn
+  port              = 443
+}
+
+resource "aws_lb_listener_rule" "forum_server_forward_rule_https" {
+  listener_arn = data.aws_lb_listener.lb_https_listener.arn
+  priority     = 1
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.forum_server_tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/api/*"]
+    }
+  }
+}
+
+
 resource "aws_lb_listener_rule" "forum_server_forward_rule" {
   listener_arn = aws_lb_listener.lb_listener.arn
   priority     = 2
