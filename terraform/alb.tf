585029cea90d89f816bd61228057297524c1a4c2
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2024-08-04 16:39:16 -0500

    remove alb resource reference

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -9,23 +9,12 @@ resource "aws_lb" "main_alb" {
   tags = local.tags
 }
 
-# resource "aws_lb_listener" "lb_listener" {
-#   load_balancer_arn = aws_lb.main_alb.arn
-#   port              = "80"
-#   protocol          = "HTTP"
-
-#   default_action {
-#     type = "fixed-response"
-#     fixed_response {
-#       status_code  = 200
-#       content_type = "text/plain"
-#       message_body = "Hello from Load Balancer listener default action"
-#     }
-#   }
-# }
+data "aws_lb" "main_lb_data" {
+  name = "main-lb"
+}
 
 data "aws_lb_listener" "lb_https_listener" {
-  load_balancer_arn = aws_lb.main_alb.arn
+  load_balancer_arn = data.aws_lb.main_lb_data.arn
   port              = 443
 }
 
@@ -44,21 +33,3 @@ resource "aws_lb_listener_rule" "forum_server_forward_rule_https" {
     }
   }
 }
-
-
-# resource "aws_lb_listener_rule" "forum_server_forward_rule" {
-#   listener_arn = aws_lb_listener.lb_listener.arn
-#   priority     = 2
-
-#   action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.forum_server_tg.arn
-#   }
-
-#   condition {
-#     // maybe change this to have an app prefix or something
-#     path_pattern {
-#       values = ["/api/*"]
-#     }
-#   }
-# }
